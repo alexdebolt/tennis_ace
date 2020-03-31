48ed34ad61dbff0d070a5428c47a00b43064267b
Alex DeBolt <58086131+alexdebolt@users.noreply.github.com>
2020-03-31 11:51:45 -0400

    Read csv data into df and exploratory analysis

--- script.py
+++ script.py
@@ -4,17 +4,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 # load and investigate the data here:
+df = pd.read_csv('./tennis_stats.csv')
 
-
-
-
+print(df.columns)
 
 
 
 
 # perform exploratory analysis here:
+plt.scatter(df['BreakPointsOpportunities'], df['Winnings'])
+plt.show()
+plt.clf()
 
-
+plt.scatter(df['DoubleFaults'], df['Winnings'])
+plt.show()
 
 
 
